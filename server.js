6f00291d081c6e22b2944cef1fc1ed147df5c95e
ZoyaBukhantseva <zoyapashchenko87@gmail.com>
2023-04-06 23:11:02 +0300

    add mongoose

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
-const app = require('./app')
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const mongoose = require("mongoose");
+
+const app = require('./app')
+const DB_HOST= "mongodb+srv://Zoya:5A2mA92NUmce5hSD@cluster0.cvnjhmr.mongodb.net/contacts_reader?retryWrites=true&w=majority"
+mongoose.connect(DB_HOST)
+.then(()=>app.listen(3000), console.log("Database connection successful"))
+.catch(error=>console.log(error.message))
+// 5A2mA92NUmce5hSD parol
