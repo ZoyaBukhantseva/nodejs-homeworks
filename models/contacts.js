c8d5cba5058a4ec8e01ed843e3a83350266ff5d6
ZoyaBukhantseva <zoyapashchenko87@gmail.com>
2023-04-20 00:57:51 +0300

    fix mistake

--- models/contacts.js
+++ models/contacts.js
@@ -18,30 +18,34 @@ const contactSchema = new Schema(
       default: false,
     }
   },
-  { versionKey: false, timestamps: true }
+  { versionKey: false }
 );
 
 
 
 const addSchema = Joi.object({
   name: Joi.string().required().messages({
-    "any.required": `"name" is required`,
+    "any.required": `missing required name field`,
     "string.empty": `"name" cannot be empty`,
     "string.base": `"name" must be string`,
   }),
   email: Joi.string().required().messages({
-    "any.required": `"email" is required`,
+    "any.required": `missing required email field`,
     "string.empty": `"email" cannot be empty`,
     "string.base": `"email" must be string`,
   }),
   phone: Joi.string().required().messages({
-    "any.required": `"phone" is required`,
+    "any.required": `missing required phone field`,
+    "string.empty": `phone cannot be empty`,
+    "string.base": `phone must be string`
   }),
   favorite: Joi.boolean(),
 });
 
 const updateFavoriteSchema = Joi.object({
-  favorite: Joi.boolean().required(),
+  favorite: Joi.boolean().required().messages({
+    "any.required": `missing required favorite field`}
+    ),
 });
 
 contactSchema.post("save", (error, data, next) => {
